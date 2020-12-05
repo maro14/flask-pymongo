27546b61ccc82c90742cfee5a335ec1bfd94aad4
Teheskhiel Fritz <60809044+maro14@users.noreply.github.com>
2020-12-05 07:51:54 +0700

    Update app.py
    add put

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ mongo = PyMongo(app)
 
 
 
-@app.route("/name", methods=["GET"])
-def get_name():
+@app.route("/user", methods=["GET"])
+def get_all_users():
 	users = mongo.db.users.find()
 	response = json_util.dumps(users)
 	return Response(response, mimetype="application/json")
 
 
 
-@app.route("/name", methods=["POST"])
-def add_name():
+@app.route("/user", methods=["POST"])
+def add_user():
 	name = request.json['name']
 
 	id = mongo.db.users.insert({
@@ -30,7 +30,17 @@ def add_name():
 	response.status_code = 201
 	return response
 
-	
+@app.route('/user/<_id>', methods=['PUT'])
+def update_user(_id):
+    name = request.json['name']
+    age = request.json['age']
+
+    mongo.db.user.update_one(
+    {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'name': name, 'age': age}})
+    response = jsonify({'messsage': 'User'+ _id +'updated'})
+    response.status_code = 200
+    return response
+
 
 @app.errorhandler(404)
 def not_found(error=None):
